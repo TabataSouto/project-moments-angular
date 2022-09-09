5adaebb910090cfd4e52faae7fdf7b2fc8bcc7ba
Tabata Souto <tabatac.souto@gmail.com>
2022-09-09 19:44:27 -0300

    feat: estruturação da arquitetura (pastas) do projeto, com as pastas de components, page, services, interfaces e images.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FormComponent } from './components/moment/form/form.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MessagesComponent,
+    FormComponent,
+    MomentFormComponent,
+    AboutComponent,
+    HomeComponent,
+    EditMomentComponent,
+    MomentComponent,
+    NewMomentComponent
   ],
   imports: [
     BrowserModule,
