c61d5978c80c7669ac15725aa501a8f2c5f22972
Tabata Souto <tabatac.souto@gmail.com>
2022-09-10 20:08:38 -0300

    feat: validação do formulário para criação de um novo momento.

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -10,9 +11,40 @@ export class MomentFormComponent implements OnInit {
   informado atravéas de "props" pelo pai. */
   @Input() btnText!: string;
 
+  momentForm!: FormGroup;
+
   constructor() { }
 
+  /* Utilizado para inicializar coisas do Angular, nesse caso,
+  precisamos inicializar o formulário */
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      // FormControl -> controla o input, no caso, o valor inputado.
+      id: new FormControl(''),
+      // [Validators.required] -> disparo de validação
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    /* a exclamação no final informa que no fim, os valores vão
+    existir, e o erro em ".invalid" sobre o valor poder ser null
+    não irá mais aparecer. */
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  /* Mesmo com validação, se não colocarmos uma trava de formulário
+  inválido, será feito o envio do form da mesma forma. Para isso,
+  é feita a condicional com return para que não seja finalizada
+  a submissão do formulário. */
+  submit() {
+    if (this.momentForm.invalid) return;
   }
 
 }
